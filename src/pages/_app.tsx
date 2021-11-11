5308c2f9571ccba4a40ef625627df9476993bc97
Mateusz Aliyev <82393815+mateuszaliyev@users.noreply.github.com>
2021-11-11 17:16:39 +0100

    Fix dependency problems

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -10,52 +10,52 @@ import { ThemeProvider } from "@material-ui/core/styles";
 import UserContextProvider from "../modules/auth/UserContextProvider";
 
 import theme from "../styles/theme";
-import "../styles/globals.scss";
+import "../styles/globals.css";
 
 const App = (props: AppProps) => {
-	const { Component, pageProps } = props;
+  const { Component, pageProps } = props;
 
-	useEffect(() => {
-		const jssStyles = document.querySelector("#jss-server-side");
-		if (jssStyles) {
-			jssStyles.parentElement?.removeChild(jssStyles);
-		}
-	}, []);
+  useEffect(() => {
+    const jssStyles = document.querySelector("#jss-server-side");
+    if (jssStyles) {
+      jssStyles.parentElement?.removeChild(jssStyles);
+    }
+  }, []);
 
-	return (
-		<>
-			<Head>
-				<link
-					href="/logo-32x32.png"
-					rel="icon"
-					type="image/png"
-					sizes="32x32"
-				/>
-				<link
-					href="/logo-512x512.png"
-					rel="icon"
-					type="image/png"
-					sizes="512x512"
-				/>
-				<meta
-					content="minimum-scale=1, initial-scale=1, width=device-width"
-					name="viewport"
-				/>
-				<title>{process.env.NEXT_PUBLIC_APP_NAME}</title>
-			</Head>
-			<ThemeProvider theme={theme}>
-				<UserContextProvider>
-					<CssBaseline />
-					<Component {...pageProps} />
-				</UserContextProvider>
-			</ThemeProvider>
-		</>
-	);
+  return (
+    <>
+      <Head>
+        <link
+          href="/logo-32x32.png"
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+        />
+        <link
+          href="/logo-512x512.png"
+          rel="icon"
+          type="image/png"
+          sizes="512x512"
+        />
+        <meta
+          content="minimum-scale=1, initial-scale=1, width=device-width"
+          name="viewport"
+        />
+        <title>{process.env.NEXT_PUBLIC_APP_NAME}</title>
+      </Head>
+      <ThemeProvider theme={theme}>
+        <UserContextProvider>
+          <CssBaseline />
+          <Component {...pageProps} />
+        </UserContextProvider>
+      </ThemeProvider>
+    </>
+  );
 };
 
 App.propTypes = {
-	Component: PropTypes.elementType.isRequired,
-	pageProps: PropTypes.object.isRequired,
+  Component: PropTypes.elementType.isRequired,
+  pageProps: PropTypes.object.isRequired,
 };
 
 export default App;
